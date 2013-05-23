172c4f6a06f278ad736067d5486d0a6f81fecbf0
HAORAN WEN <hwen@zynga.com>
2013-05-22 17:53:27 -0700

    second

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //more
     ///lololo
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
